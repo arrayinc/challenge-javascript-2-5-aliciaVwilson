c6d8b95891ccdcd47dfef1f528f8f9fce8d9c53e
Alicia Wilson <alicia@thearrayschool.com>
2018-12-05 15:08:06 -0700

    finished challenge 5

--- answer.js
+++ answer.js
@@ -1,1 +1,12 @@
 // *** YOUR ANSWER BELOW ***
+function negate(word){
+  return "un" + word;
+}
+
+function intensify(word){
+  return "plus" + word;
+}
+
+function reinforce(input){
+    return "double" + input;
+}
